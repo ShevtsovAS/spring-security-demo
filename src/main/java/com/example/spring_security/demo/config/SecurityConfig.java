18b90e2fc7694e5b9b150da6ed33de55fae34332
Alexandr Shevtsov <ashevtsov@luxoft.com>
2021-03-25 01:42:08 +0300

    выбор типа авторизации через конфигурацию

--- src/main/java/com/example/spring_security/demo/config/SecurityConfig.java
+++ src/main/java/com/example/spring_security/demo/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.example.spring_security.demo.config;
 
 import com.example.spring_security.demo.security.JwtTokenFilter;
+import com.example.spring_security.demo.security.SecurityType;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,19 +15,61 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
+@Profile("security")
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Value("${security-type:base}")
+    private SecurityType securityType;
+
     private final JwtTokenFilter jwtTokenFilter;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        switch (securityType) {
+            case BASE -> configureBase(http);
+            case FORM -> configureForm(http);
+            case JWT -> configureJwt(http);
+        }
+    }
+
+    private void configureBase(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    private void configureForm(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/auth/login").permitAll()
+                .defaultSuccessUrl("/auth/success")
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/auth/logout", "POST"))
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .deleteCookies("JSESSIONID")
+                .logoutSuccessUrl("/auth/login");
+    }
+
+    private void configureJwt(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .httpBasic().disable()
---
+++ src/main/java/com/example/spring_security/demo/security/SecurityType.java
@@ -0,0 +1,5 @@
+package com.example.spring_security.demo.security;
+
+public enum SecurityType {
+    BASE, FORM, JWT
+}
